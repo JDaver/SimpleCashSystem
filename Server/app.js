27e846dd5d7dfab537153daafeee871c249a8554
JDaver <gundogdudaver@gmail.com>
2025-08-03 12:47:45 +0200

    stage db.js

--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const pool = require('./db/db');
 
 const reactPath = path.join(__dirname, '..', 'Views', 'dist');
+
 app.use(express.static(reactPath));
+
+app.get(/.*/,async(req,res,next)=>{
+  try{
+    const jsontest = await pool.query("SELECT NOW()");
+    console.log("current date: " +jsontest.rows[0].now);
+  }catch(err){
+    console.log("errore " + err.message);
+  }
+  next(); 
+});
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(reactPath, 'index.html'));

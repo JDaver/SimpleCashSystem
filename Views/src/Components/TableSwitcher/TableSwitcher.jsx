4c5e9824ceb93ca3f15daca534e11b848ac6f2e3
Daniele <ddaaaniii.98@gmail.com>
2025-08-13 11:05:11 +0200

    feat(components): add TableSwitcher for use across pages

---
+++ Views/src/Components/TableSwitcher/TableSwitcher.jsx
@@ -0,0 +1,13 @@
+import Table from '../Table/Table';
+import TableGroup from '../TableGroup/TableGroup';
+
+function TableSwitcher() {
+  return (
+    <TableGroup defaultActive={'box1'}>
+      <Table id="box1" title="Modifica Articoli" />
+      <Table id="box2" title="Inserisci un nuovo articolo" />
+    </TableGroup>
+  );
+}
+
+export default TableSwitcher;

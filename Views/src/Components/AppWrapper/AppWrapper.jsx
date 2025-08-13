a32877dc9b4070ce4dc66d80d9287412e26e8eb3
Daniele <ddaaaniii.98@gmail.com>
2025-08-13 10:08:49 +0200

    feat(components): update structure for AppWrapper to include routes and and basic layout

--- Views/src/Components/AppWrapper/AppWrapper.jsx
+++ Views/src/Components/AppWrapper/AppWrapper.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+import PageWrapper from '../PageWrapper/PageWrapper';
+import ManageItem from '../../pages/ManageItem';
+import Collection from '../../pages/Collection';
+import Terminate from '../../pages/TerminateSession';
+import Home from '../../pages/Home';
+import NotFound from '../../pages/NotFound';
 
 function AppWrapper() {
   const location = useLocation();
   const [currentLocation, setCurrentLocation] = useState(location);
 
   useEffect(() => {
-    if (!document.startViewTransition || currentLocation === location) {
+    if (!document.startViewTransition || currentLocation.key === location.key) {
       setCurrentLocation(location);
       return;
     }
@@ -18,11 +25,21 @@ function AppWrapper() {
     return () => {
       transition?.finished?.catch(() => {});
     };
-  }, [location, currentLocation]);
+  }, [location]);
 
   return (
     <div id="view-wrapper">
-      <Outlet />
+      <Navbar />
+      <PageWrapper>
+        <Routes location={currentLocation}>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Navigate to="/" replace />} />
+          <Route path="/ManageItem" element={<ManageItem />} />
+          <Route path="/Collection" element={<Collection />} />
+          <Route path="/TerminateSession" element={<Terminate />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </PageWrapper>
     </div>
   );
 }

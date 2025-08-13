42cdfebbcacd17cbe1813f7f5a2efb8067cdacc8
Daniele <ddaaaniii.98@gmail.com>
2025-08-13 11:12:44 +0200

    feat(components): rework Receipt styling classes
    As of right now is tested with static values

---
+++ Views/src/Components/Receipt/Receipt.jsx
@@ -0,0 +1,34 @@
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <div className="receipt__table-wrapper">
+        <table className="receipt__content">
+          <thead>
+            <tr>
+              <th>Articolo</th>
+              <th>Quantita</th>
+              <th>Prezzo</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="receipt__item">
+              <td className="receipt__item-name">pizza</td>
+              <td className="receipt__item-quantity">1</td>
+              <td className="receipt__item-price">$6.0</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr className="receipt__total">
+              <td colSpan="2">TOTALE</td>
+              <td>$100</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Receipt;

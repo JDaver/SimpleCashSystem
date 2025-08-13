44a9353d9adf7579d94aba3eb1528261635ee56f
Daniele <ddaaaniii.98@gmail.com>
2025-08-13 11:49:18 +0200

    refactor: rework app structure and add custom font

--- Views/src/App.jsx
+++ Views/src/App.jsx
@@ -1,33 +1,12 @@
-import MyNavBar from "./Components/navBar";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import ManageItem from "./pages/ManageItem";
-import Collection from "./pages/Collection";
-import Terminate from "./pages/TerminateSession";
-import Home from "./pages/Home";
-import NotFound from './pages/NotFound';
+import { BrowserRouter } from 'react-router-dom';
+import AppWrapper from './components/AppWrapper/AppWrapper';
 
 function App() {
+  return (
+    <BrowserRouter>
+      <AppWrapper />
+    </BrowserRouter>
+  );
+}
 
-    return (
-    <>
-      <div>
-        <BrowserRouter>
-          <MyNavBar/>
-          <Routes>
-            <Route path="/Home" element={<Home/>}/>
-            <Route path="/ManageItem" element={<ManageItem/>}/>
-            <Route path="/Collection" element={<Collection/>}/>
-            <Route path="/TerminateSession" element={<Terminate/>}/>
-            <Route path="*" element={<NotFound/>}/>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </>
-    
-  )
-
-  }
-  
-
-
-export default App
+export default App;

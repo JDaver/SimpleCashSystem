9a0c2643301c2dd73547ac574390540af527b172
Daniele <ddaaaniii.98@gmail.com>
2025-08-19 23:20:48 +0200

    feat(Item): refactor css file

--- Views/src/Components/Item/Item.jsx
+++ Views/src/Components/Item/Item.jsx
@@ -3,7 +3,7 @@ import { PlusIcon, MinusIcon } from '@heroicons/react/24/outline';
 
 function Item({ name }) {
   return (
-    <div className="item">
+    <li className="item">
       <p className="item__name">{name}</p>
       <div className="item__btns-container">
         <button type="button" className="item__plus-btn">
@@ -13,7 +13,7 @@ function Item({ name }) {
           <MinusIcon />
         </button>
       </div>
-    </div>
+    </li>
   );
 }
 

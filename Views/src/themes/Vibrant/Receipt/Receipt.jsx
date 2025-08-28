4f9c5e3edf92f593bb631f4d997aaaca3924a2dd
JDaver <gundogdudaver@gmail.com>
2025-08-28 13:14:06 +0200

    restored controlButtons of receipt initial layout

--- Views/src/themes/Vibrant/Receipt/Receipt.jsx
+++ Views/src/themes/Vibrant/Receipt/Receipt.jsx
@@ -3,7 +3,7 @@ import TableReceipt from "./TableReceipt"
 import ControlButtons from "./ControlButtons"
 export default function Receipt(){
     return(
-    <div className="Receipt_wrapper">
+    <div className="receipt_wrapper">
         <TableReceipt/>
         <ControlButtons/>
         </div>

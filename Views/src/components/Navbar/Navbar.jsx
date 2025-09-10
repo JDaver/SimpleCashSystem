c0190a66af6bb7703684b2fa6666406dbdb8c796
Daniele <ddaaaniii.98@gmail.com>
2025-09-10 16:45:07 +0200

    feat(Header): introduce Dropdown for settings in Navbar

--- Views/src/components/Navbar/Navbar.jsx
+++ Views/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,26 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTheme } from '../../contexts/useTheme';
 import './Navbar.css';
+import { Cog6ToothIcon } from '@heroicons/react/24/outline';
+import Dropdown from '@components/Dropdown';
 
+const navLocations = [
+  { id: 'main', label: 'Cassa', to: '/' },
+  { id: 'manage', label: 'Modifica articoli', to: '/ManageItem' },
+  { id: 'collection', label: 'Storico', to: '/Collection' },
+  // { id: 'settings', label: 'Impostazioni', to: '/SettingsPage' },
+];
+
+const settingsArr = [
+  { id: 1, name: 'Generali' },
+  { id: 2, name: 'Utente' },
+  { id: 3, name: 'Scarica' },
+  { id: 4, name: 'Elimina' },
+];
 function Navbar() {
   const { theme } = useTheme();
-  const navLocations = [
-    { id: 'main', label: 'Cassa', to: '/' },
-    { id: 'manage', label: 'Modifica articoli', to: '/ManageItem' },
-    { id: 'collection', label: 'Storico', to: '/Collection' },
-    { id: 'settings', label: 'Impostazioni', to: '/SettingsPage' },
-  ];
+  const [settings, setSettings] = useState('');
 
   return (
     <nav className={`navbar ${theme}`}>
@@ -27,6 +37,21 @@ function Navbar() {
             </NavLink>
           </li>
         ))}
+        <Dropdown side={'left'} selected={settings} onChange={setSettings}>
+          <Dropdown.Trigger>
+            <Cog6ToothIcon color="white" width={30} height={20} />
+            <span style={{ color: 'white' }}>Impostazioni</span>
+          </Dropdown.Trigger>
+          <Dropdown.Content>
+            {settingsArr.map(setting => {
+              return (
+                <Dropdown.Item key={setting.id} option={setting}>
+                  {setting.name}
+                </Dropdown.Item>
+              );
+            })}
+          </Dropdown.Content>
+        </Dropdown>
       </ul>
     </nav>
   );

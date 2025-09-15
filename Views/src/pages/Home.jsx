8639f4ab3603d34f7a2d8df19646c5eac0ec8568
Daniele <ddaaaniii.98@gmail.com>
2025-09-15 16:18:02 +0200

    refactor(Theme): update import path for context

--- Views/src/pages/Home.jsx
+++ Views/src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import { useMemo } from 'react';
 import { loadThemedComponent } from '@utils/LoadThemedComponent';
-import { useTheme } from '@contexts/useTheme';
-import {ReceiptProvider} from '@contexts/receiptHandlerContext';
+import { useTheme } from '@contexts/Theme';
+import { ReceiptProvider } from '@contexts/receiptHandlerContext';
 function Home() {
-
-const { theme } = useTheme();
+  const { theme } = useTheme();
   const CashierScreen = useMemo(() => loadThemedComponent(theme, 'CashierScreen'), [theme]);
-  const Receipt = useMemo(() => loadThemedComponent(theme, 'Receipt'),[theme]);
+  const Receipt = useMemo(() => loadThemedComponent(theme, 'Receipt'), [theme]);
   return (
     <ReceiptProvider>
       <CashierScreen />

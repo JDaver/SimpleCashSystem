0c47dd9e11ce8c2bf1fa7a26cd6e542c85f1269d
Daniele <ddaaaniii.98@gmail.com>
2025-10-04 01:55:43 +0200

    feat(Settings): add subroutes pages

--- Views/src/pages/SettingsPage.jsx
+++ Views/src/pages/SettingsPage.jsx
@@ -1,8 +1,29 @@
-import Settings from "../themes/Vibrant/Settings/Settings";
+import { useMemo } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useTheme } from '@contexts/Theme';
+import { loadThemedComponent } from '@utils/loadThemedComponent';
+import General from '@themes/Minimal/Settings/Roots/General';
+import Receipts from '@themes/Minimal/Settings/Roots/Receipts';
+import Users from '@themes/Minimal/Settings/Roots/Users';
+import Download from '@themes/Minimal/Settings/Roots/Download';
+import Delete from '@themes/Minimal/Settings/Roots/Delete';
+
 function SettingsPage() {
+  const { theme } = useTheme();
+  const Settings = useMemo(() => loadThemedComponent(theme, 'Settings'), [theme]);
+
   return (
-    <Settings/>
-  )
+    <Routes>
+      <Route path="/" element={<Settings />}>
+        <Route index element={<Navigate to="general" replace />} />
+        <Route path="general" element={<General />} />
+        <Route path="receipts" element={<Receipts />} />
+        <Route path="users" element={<Users />} />
+        <Route path="download" element={<Download />} />
+        <Route path="delete" element={<Delete />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default SettingsPage;

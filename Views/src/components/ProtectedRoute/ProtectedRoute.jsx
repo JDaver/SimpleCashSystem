65a1b02de3c840f3eb09641bacfa463a7c6e4c3e
Daniele <ddaaaniii.98@gmail.com>
2025-10-06 23:56:50 +0200

    feat(Auth): create ProtectedRoute component

---
+++ Views/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthContext } from '../../contexts/Auth';
+
+function ProtectedRoute() {
+  const { session } = useAuthContext();
+
+  return session ? <Outlet /> : <Navigate to="/login" replace />;
+}
+
+export default ProtectedRoute;

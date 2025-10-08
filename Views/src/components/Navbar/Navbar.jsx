b796e1c31e9960ed9039e2b946b41d258f80d885
Daniele <ddaaaniii.98@gmail.com>
2025-10-08 14:42:44 +0200

    feat(Navbar): improve component structure

--- Views/src/components/Navbar/Navbar.jsx
+++ Views/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,17 @@
-import { memo, useState } from 'react';
+import { memo } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTheme } from '@contexts/Theme';
 import './Navbar.css';
-import { Cog6ToothIcon } from '@heroicons/react/24/outline';
-import Dropdown from '@components/Dropdown';
 
 const navLocations = [
   { id: 'main', label: 'Cassa', to: '/' },
-  { id: 'manage', label: 'Modifica articoli', to: '/ManageItem' },
-  { id: 'collection', label: 'Storico', to: '/Collection' },
-  // { id: 'settings', label: 'Impostazioni', to: '/SettingsPage' },
+  { id: 'manage', label: 'Modifica articoli', to: '/manageitem' },
+  { id: 'collection', label: 'Storico', to: '/collection' },
+  { id: 'settings', label: 'Impostazioni', to: '/settings' },
 ];
 
-const settingsArr = [
-  { id: 1, name: 'Generali' },
-  { id: 2, name: 'Utente' },
-  { id: 3, name: 'Scarica' },
-  { id: 4, name: 'Elimina' },
-];
 function Navbar() {
   const { theme } = useTheme();
-  const [settings, setSettings] = useState('');
 
   return (
     <nav className={`navbar ${theme}`}>
@@ -31,27 +22,12 @@ function Navbar() {
               key={location.id}
               className={({ isActive }) => (isActive ? 'navbar__link active' : 'navbar__link')}
               to={location.to}
-              end
+              end={location.id !== 'settings'}
             >
               {location.label}
             </NavLink>
           </li>
         ))}
-        <Dropdown side={'left'} selected={settings} onChange={setSettings}>
-          <Dropdown.Trigger>
-            <Cog6ToothIcon color="white" width={30} height={20} />
-            <span style={{ color: 'white' }}>Impostazioni</span>
-          </Dropdown.Trigger>
-          <Dropdown.Content>
-            {settingsArr.map(setting => {
-              return (
-                <Dropdown.Item key={setting.id} option={setting}>
-                  {setting.name}
-                </Dropdown.Item>
-              );
-            })}
-          </Dropdown.Content>
-        </Dropdown>
       </ul>
     </nav>
   );

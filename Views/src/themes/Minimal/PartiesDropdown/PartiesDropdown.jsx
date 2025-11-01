276db5405cd256ef50bd17e4aaac97e7b6b14410
JDaver <gundogdudaver@gmail.com>
2025-11-01 19:09:07 +0100

    add backend check validation for product and minor fix on tableAnimation change

--- Views/src/themes/Minimal/PartiesDropdown/PartiesDropdown.jsx
+++ Views/src/themes/Minimal/PartiesDropdown/PartiesDropdown.jsx
@@ -2,9 +2,11 @@ import Dropdown from '@components/Dropdown';
 import { CheckIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 import { usePartyNames } from '../../../Hooks/productsHook';
 import { memo } from 'react';
+import { useInsertItem } from '../InsertItem/useInsertItem';
 
 function PartiesDropdown({ disabled, parties, setParties }) {
   const partyNames = usePartyNames();
+  const { partiesRelated } = useInsertItem();
   return (
     <Dropdown disabled={disabled} selected={parties} onChange={setParties} multiple>
       <Dropdown.Trigger>
@@ -15,9 +17,7 @@ function PartiesDropdown({ disabled, parties, setParties }) {
         {partyNames.map(party => (
           <Dropdown.Item key={party.party_id} option={party.party_id}>
             <span className="check-icon-wrapper">
-              {productDetails.partiesRelated.includes(party.party_id) && (
-                <CheckIcon width={30} height={20} />
-              )}
+              {partiesRelated.includes(party.party_id) && <CheckIcon width={30} height={20} />}
             </span>
             <span>{party.name_party}</span>
           </Dropdown.Item>

f38091023bbb94f386b653ba4cf68242a0c3dc90
Daniele <ddaaaniii.98@gmail.com>
2025-11-04 23:33:12 +0100

    feat(Layout): add Footer to component structure

--- Views/src/components/Layout/Layout.jsx
+++ Views/src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import PageWrapper from '@components/PageWrapper';
 import Header from '@components/Header';
+import Footer from '../Footer';
 
 function Layout() {
   return (
@@ -12,6 +13,7 @@ function Layout() {
           <Outlet />
         </Suspense>
       </PageWrapper>
+      <Footer />
     </div>
   );
 }

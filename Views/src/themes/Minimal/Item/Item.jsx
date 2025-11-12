f0c6380da8e2ab80a71240d4f7cccbd96773988b
Daniele <ddaaaniii.98@gmail.com>
2025-11-12 12:21:30 +0100

    feat(Item): make component reusable and support render props for info/actions

--- Views/src/themes/Minimal/Item/Item.jsx
+++ Views/src/themes/Minimal/Item/Item.jsx
@@ -1,12 +1,22 @@
 import { memo } from 'react';
 import './Item.css';
 
-function Item({ className, renderInfo, renderActions, children, ...props }) {
+function Item({
+  className,
+  gap,
+  gridTemplateColumns,
+  renderInfo,
+  renderActions,
+  children,
+  ...props
+}) {
   const combinedClassName = ['item', className].filter(Boolean).join(' ');
   return (
     <div className={combinedClassName} {...props}>
       {children}
-      <div className="item__info">{renderInfo?.()}</div>
+      <div className="item__info" style={{ gap, gridTemplateColumns }}>
+        {renderInfo?.()}
+      </div>
       <div className="item__actions">{renderActions?.()}</div>
     </div>
   );

e619c0c8dfd9fa1c556744289bcee60ffc5629ce
Daniele <ddaaaniii.98@gmail.com>
2025-11-25 01:10:02 +0100

    feat(ReceiptsHistory): split into subcomponents receipts actions and info

--- Views/src/themes/Minimal/ReceiptsHistory/ReceiptsHistory.jsx
+++ Views/src/themes/Minimal/ReceiptsHistory/ReceiptsHistory.jsx
@@ -2,12 +2,24 @@ import { memo, useCallback } from 'react';
 import { ReceiptPercentIcon } from '@heroicons/react/24/outline';
 import { useCollectionProvider } from '@contexts/CollectionItem/collectionContext';
 import { useFetchReceipts } from '@hooks/receiptHook';
+import { useInfiniteScroll } from '@hooks/useInfiniteScroll';
 import { formatDate, formatPrice } from '@utils/helpers';
 import DisplayElements from '@themes/Minimal/DisplayElements';
 import Item from '@themes/Minimal/Item';
+import './ReceiptHistory.css';
+import Info from '../Info';
 
-function ReceiptsHistory() {
-  const { receipts } = useFetchReceipts();
+const ReceiptInfo = memo(function ReceiptInfo({ id, date, total }) {
+  return (
+    <Info>
+      <span>{id}</span>
+      <span>{formatDate(date)}</span>
+      <span>{formatPrice(total.toString().replace('.', ','))}</span>
+    </Info>
+  );
+});
+
+const ReceiptActions = memo(function ReceiptActions({ receipt }) {
   const { setItemsInReceipt, setTitle } = useCollectionProvider();
 
   const handleModal = useCallback(receipt => {
@@ -16,32 +28,32 @@ function ReceiptsHistory() {
   }, []);
 
   return (
+    <button className="receipt__summary-btn" onClick={() => handleModal(receipt)}>
+      <ReceiptPercentIcon width={30} height={30} />
+    </button>
+  );
+});
+
+function ReceiptsHistory() {
+  const { receipts, fetchNext, hasMoreNext } = useFetchReceipts();
+  const { bottomLoaderRef } = useInfiniteScroll(fetchNext, hasMoreNext);
+
+  return (
     <DisplayElements labels={['N. Scontrino', 'Data', 'Totale']} actionLabels={['Riepilogo']}>
       {receipts.map(receipt => {
         return (
           <Item
             key={receipt.id}
             renderInfo={() => {
-              return (
-                <div
-                  style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 250px)', gap: '6rem' }}
-                >
-                  <span>{receipt.id}</span>
-                  <span>{formatDate(receipt.date)}</span>
-                  <span>{formatPrice(receipt.total.toString().replace('.', ','))}</span>
-                </div>
-              );
+              return <ReceiptInfo id={receipt.id} date={receipt.date} total={receipt.total} />;
             }}
             renderActions={() => {
-              return (
-                <button style={{ marginRight: '1.5rem' }} onClick={() => handleModal(receipt)}>
-                  <ReceiptPercentIcon width={30} height={30} />
-                </button>
-              );
+              return <ReceiptActions receipt={receipt} />;
             }}
           />
         );
       })}
+      <div ref={bottomLoaderRef} className="receipt__loader" />
     </DisplayElements>
   );
 }

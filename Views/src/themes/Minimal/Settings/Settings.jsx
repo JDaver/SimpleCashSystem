544de198545aa1240b5fc400ffe9e4124ca0acd3
Daniele <ddaaaniii.98@gmail.com>
2025-11-29 20:48:03 +0100

    feat(Settings): integrate DotsLoader component

--- Views/src/themes/Minimal/Settings/Settings.jsx
+++ Views/src/themes/Minimal/Settings/Settings.jsx
@@ -1,15 +1,17 @@
-import { Link, Outlet, useLocation } from 'react-router-dom';
-import { memo } from 'react';
+import { Outlet } from 'react-router-dom';
+import { memo, Suspense } from 'react';
+import SettingsSidebar from '@components/SettingsSidebar';
+import DotsLoader from '@components/DotsLoader';
 import './Settings.css';
-import SettingsSidebar from '../../../components/SettingsSidebar';
 
 function Settings() {
   return (
     <div className="settings__wrapper">
       <SettingsSidebar />
-
       <div className="settings__content">
-        <Outlet />
+        <Suspense fallback={<DotsLoader />}>
+          <Outlet />
+        </Suspense>
       </div>
     </div>
   );
